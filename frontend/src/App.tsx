c68d073748a6c4b20495dbc283a3ebd4cb9efaac
JuanBarcelos <juanbarcelos@outlook.com.br>
2021-12-18 12:00:43 -0300

    create frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './component/cards/cards';
+import Footer from './component/footer/footer';
+import Header from './component/header/header';
+
+
+function App() {
+  return (
+    <div className=''>
+        <Header />
+        <Card />
+        <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/component/header/header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="text-center pt-3">
+      <h1 className="">Previsão de Chuva Horária</h1>
+    </div>
+
+  );
+}
+
+export default Header;
