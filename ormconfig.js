afb82915ae27421490c26d16ea2888e511fb9892
JuanBarcelos <juanbarcelos@outlook.com.br>
2022-01-27 15:51:37 -0300

    alteração da porta

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,4 @@
+/*
 const config = {
     type: 'postgres',
     url: 'postgres://xjxbcfqcacazge:16d903047a3b022280caa6b0d1605c294aba09122c08f35b2f9b3241f56c706e@ec2-34-205-46-149.compute-1.amazonaws.com:5432/db6t79p34o01gb',
@@ -21,3 +22,4 @@ const config = {
  };
  
  module.exports = config;
+ */
